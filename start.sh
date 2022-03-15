8bf441405523974d203df5816eaf1a2dfd66a178
deddy <fransiscus.deddy@gmail.com>
2022-03-15 19:02:56 +0700

    update git cache

--- start.sh
+++
@@ -1,7 +0,0 @@
-export DATABASE_HOST="localhost"
-export DATABASE_PORT=5432
-export DATABASE_USER="postgres"
-export DATABASE_PASS="paramadaksa"
-export DATABASE_DBNAME="antarBarang"
-
-go run main.go
